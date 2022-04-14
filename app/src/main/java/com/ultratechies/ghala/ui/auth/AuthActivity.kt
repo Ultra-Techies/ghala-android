fbde7940c94e1e33cd5f20997c6b187a5b38592f
Florence Njumbi <wambuinjumbi@gmail.com>
2022-04-14 14:26:19 +0300

    refactor:  code clean up

--- app/src/main/java/com/ultratechies/ghala/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/ultratechies/ghala/ui/auth/AuthActivity.kt
@@ -1,12 +1,12 @@
 package com.ultratechies.ghala.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.ultratechies.ghala.databinding.ActivityAuthBinding
 
 
 class AuthActivity : AppCompatActivity() {
-  private lateinit var binding : ActivityAuthBinding
+    private lateinit var binding: ActivityAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
