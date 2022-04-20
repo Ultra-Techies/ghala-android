1c342e4be708151ebf9a1dd2f2dacf982129eb1d
malcolmmaima <malcolmmaima@gmail.com>
2022-04-20 12:31:06 +0300

    minor

--- app/src/main/java/com/ultratechies/ghala/data/models/responses/Warehouse.kt
+++ app/src/main/java/com/ultratechies/ghala/data/models/responses/Warehouse.kt
@@ -2,6 +2,6 @@ package com.ultratechies.ghala.data.models.responses
 
 data class Warehouse (
     val id: Int,
-    val location: String,
-    val name: String
+    val name: String,
+    val location: String
 )
