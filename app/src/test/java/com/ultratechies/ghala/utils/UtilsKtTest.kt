ccdc93087f7c0700b6d2e20f86b817d2506149fa
Florence Njumbi <wambuinjumbi@gmail.com>
2022-04-26 00:54:28 +0300

    refactor: code clean up

---
+++ app/src/test/java/com/ultratechies/ghala/utils/UtilsKtTest.kt
@@ -0,0 +1,18 @@
+package com.ultratechies.ghala.utils
+
+import org.junit.Test
+
+class UtilsKtTest {
+
+//    @Test
+//    fun returnFormattedDate() {
+//        val fDate =
+//            formatDate(
+//                "2022-04-30T00:00:00.000+00:00",
+//                "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'XXX",
+//                "dd-MM-yyyy"
+//            )
+//        assert(fDate == "30-04-2022")
+//    }
+
+}
