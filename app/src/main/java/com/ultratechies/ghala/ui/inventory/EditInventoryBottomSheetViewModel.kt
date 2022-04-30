ca82bb137718759372486bb792bbbb4efac974ff
Florence Njumbi <wambuinjumbi@gmail.com>
2022-04-30 21:20:12 +0300

    feature: edit inventory item

---
+++ app/src/main/java/com/ultratechies/ghala/ui/inventory/EditInventoryBottomSheetViewModel.kt
@@ -0,0 +1,42 @@
+package com.ultratechies.ghala.ui.inventory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ultratechies.ghala.data.models.requests.EditInventoryRequest
+import com.ultratechies.ghala.data.repository.APIResource
+import com.ultratechies.ghala.data.repository.InventoryRepository
+import com.ultratechies.ghala.utils.parseErrors
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EditInventoryBottomSheetViewModel @Inject constructor(val inventoryRepository: InventoryRepository) : ViewModel() {
+
+    private val _editInventory = MutableSharedFlow<Any>()
+    val editInventory = _editInventory.asSharedFlow()
+
+    private val _errorMessage = MutableSharedFlow<String>()
+    val errorMessage =_errorMessage.asSharedFlow()
+
+    fun editInventoryItem(editInventoryRequest: EditInventoryRequest){
+        viewModelScope.launch {
+            val response = inventoryRepository.editInventoryItem(editInventoryRequest)
+            when(response){
+                is APIResource.Success ->{
+                    _editInventory.emit(response.value)
+                }
+                is APIResource.Error ->{
+                    _errorMessage.emit(parseErrors(response))
+                }
+                else ->{
+
+                }
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/ultratechies/ghala/ui/inventory/InventoryFragment.kt
+++ app/src/main/java/com/ultratechies/ghala/ui/inventory/InventoryFragment.kt
@@ -1,9 +1,12 @@
 package com.ultratechies.ghala.ui.inventory
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -12,8 +15,8 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
+import com.ultratechies.ghala.data.models.responses.inventory.InventoryResponseItem
 import com.ultratechies.ghala.databinding.InventoryFragmentBinding
-import com.ultratechies.ghala.ui.orders.OrdersFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -23,9 +26,7 @@ class InventoryFragment : Fragment() {
     private lateinit var inventoryAdapter: InventoryAdapter
     private lateinit var binding: InventoryFragmentBinding
 
-    companion object {
-        fun newInstance() = OrdersFragment()
-    }
+    private var data = mutableListOf<InventoryResponseItem>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,17 +39,58 @@ class InventoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        this.requireActivity()
+
         setupInventoryAdapter()
+        getInventory()
+
         binding.swipeContainer.isRefreshing = false
-        viewModel.fetchInventory()
         fetchInventoryListener()
         fetchInventoryErrorListener()
+        deleteInventoryListener()
+        deleteInventoryErrorListener()
         onRefresh()
+
+        binding.addNewInventory.setOnClickListener {
+            val addInventoryBottomSheet = AddInventoryBottomFragment {
+                getInventory()
+            }
+            addInventoryBottomSheet.show(childFragmentManager, AddInventoryBottomFragment.TAG)
+        }
+        binding.spinnerCategoryStatus.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (position == 0) {
+                        displayData(data)
+                    } else {
+                        val filter =
+                            data.filter {
+                                it.status.lowercase() == binding.spinnerCategoryStatus.selectedItem.toString()
+                                    .lowercase()
+                            }
+                        displayData(filter)
+                    }
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+
+                }
+            }
+    }
+
+
+    private fun getInventory() {
+        viewModel.fetchInventory()
     }
 
     private fun onRefresh() {
         binding.swipeContainer.setOnRefreshListener {
-            viewModel.fetchInventory()
+            getInventory()
         }
     }
 
@@ -69,24 +111,92 @@ class InventoryFragment : Fragment() {
 
     private fun fetchInventoryListener() {
         viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
                 viewModel.fetchInventory.collect {
                     binding.swipeContainer.isRefreshing = false
-                    if (it.isEmpty()) {
-                        binding.tvEmptyInventoryItems.visibility = View.VISIBLE
-                    } else {
-                        inventoryAdapter.saveData(it)
-                    }
+                    data.clear()
+                    data.addAll(it)
+                    displayData(it)
                 }
             }
         }
     }
 
+    private fun displayData(list: List<InventoryResponseItem>) {
+        if (list.isEmpty()) {
+            binding.tvEmptyInventoryItems.visibility = View.VISIBLE
+            binding.recyclerViewInventory.visibility = View.GONE
+        } else {
+            binding.recyclerViewInventory.visibility = View.VISIBLE
+            binding.tvEmptyInventoryItems.visibility = View.GONE
+            inventoryAdapter.saveData(list)
+            binding.recyclerViewInventory.scrollToPosition(
+                0
+            )
+        }
+    }
+
     private fun setupInventoryAdapter() {
         inventoryAdapter = InventoryAdapter()
-        val recyclerView = binding.recyclerViewInventory
-        recyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
-        recyclerView.adapter = inventoryAdapter
+        inventoryAdapter.onItemClick { inventoryResponseItem ->
+            val editBtmFrag = EditInventoryBottomFragment.newInstance(inventoryResponseItem)
+            editBtmFrag.onRefresh {
+                // make network
+                getInventory()
+            }
+            editBtmFrag.show(childFragmentManager, "edit_frag")
+        }
+        inventoryAdapter.onItemDelete {
+            deleteInventoryItem(sku = it.sku)
+            Log.d("SKU", it.sku.toString())
+        }
+
+        binding.recyclerViewInventory.apply {
+            adapter = inventoryAdapter
+            layoutManager =
+                LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        }
+
+    }
+
+    private fun deleteInventoryItem(sku: Int) {
+        viewModel.deleteInventory(sku)
+    }
+
+    private fun deleteInventoryListener() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.deleteInventory.collect {
+                    getInventory()
+                    binding.swipeContainer.isRefreshing = false
+                    Toast.makeText(
+                        requireContext(),
+                        "Task Deleted Successfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun deleteInventoryErrorListener() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.errorMessage.collect {
+                    binding.swipeContainer.isRefreshing = false
+                    Toast.makeText(
+                        requireContext(),
+                        it,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getInventory()
     }
 
 
