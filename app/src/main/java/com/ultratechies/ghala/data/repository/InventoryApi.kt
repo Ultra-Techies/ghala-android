4d45352487dcbb6515e406e53c77d817001ba48f
Florence Njumbi <wambuinjumbi@gmail.com>
2022-04-30 21:18:35 +0300

    feature: hit api end point

--- app/src/main/java/com/ultratechies/ghala/data/repository/InventoryApi.kt
+++ app/src/main/java/com/ultratechies/ghala/data/repository/InventoryApi.kt
@@ -1,11 +1,29 @@
 package com.ultratechies.ghala.data.repository
 
-import com.ultratechies.ghala.data.models.responses.InventoryResponseItem
-import retrofit2.http.GET
+import com.ultratechies.ghala.data.models.requests.AddInventoryRequest
+import com.ultratechies.ghala.data.models.requests.EditInventoryRequest
+import com.ultratechies.ghala.data.models.responses.inventory.AddNewInventoryResponse
+import com.ultratechies.ghala.data.models.responses.inventory.InventoryResponseItem
+import retrofit2.http.*
 
 interface InventoryApi {
 
     @GET("allinventory")
     suspend fun getInventory(): List<InventoryResponseItem>
+
+    @POST("inventory")
+    suspend fun addInventoryItem(
+        @Body addInventoryItem: AddInventoryRequest
+    ): AddNewInventoryResponse
+
+    @PUT("inventory")
+    suspend fun editInventoryItem(
+        @Body editInventoryRequest: EditInventoryRequest
+    )
+
+    @DELETE("inventory/{sku}")
+    suspend fun deleteInventoryItem(
+        @Path("sku") sku: Int
+    )
 
 }
--- app/src/main/java/com/ultratechies/ghala/ui/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/ultratechies/ghala/ui/inventory/InventoryViewModel.kt
@@ -2,7 +2,7 @@ package com.ultratechies.ghala.ui.inventory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.ultratechies.ghala.data.models.responses.InventoryResponseItem
+import com.ultratechies.ghala.data.models.responses.inventory.InventoryResponseItem
 import com.ultratechies.ghala.data.repository.APIResource
 import com.ultratechies.ghala.data.repository.InventoryRepository
 import com.ultratechies.ghala.utils.parseErrors
@@ -13,31 +13,50 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class InventoryViewModel @Inject constructor( val inventoryRepo: InventoryRepository) : ViewModel() {
+class InventoryViewModel @Inject constructor(val inventoryRepo: InventoryRepository) : ViewModel() {
 
     private val _fetchInventory = MutableSharedFlow<List<InventoryResponseItem>>()
     val fetchInventory = _fetchInventory.asSharedFlow()
 
+    private val _deleteInventory = MutableSharedFlow<Any>()
+    val deleteInventory = _deleteInventory.asSharedFlow()
+
     private val _errorMessage = MutableSharedFlow<String>()
     val errorMessage = _errorMessage.asSharedFlow()
 
-    fun fetchInventory(){
+    fun fetchInventory() {
         viewModelScope.launch {
             val response = inventoryRepo.getInventory()
-            when(response){
-                is APIResource.Success ->{
-                    _fetchInventory.emit(response.value)
+            when (response) {
+                is APIResource.Success -> {
+                    _fetchInventory.emit(response.value.reversed())
                 }
-                is APIResource.Loading ->{
+                is APIResource.Loading -> {
 
                 }
-                is APIResource.Error ->{
+                is APIResource.Error -> {
                     _errorMessage.emit(parseErrors(response))
                 }
             }
         }
     }
 
+    fun deleteInventory(sku: Int) {
+        viewModelScope.launch {
+            val response = inventoryRepo.deleteInventoryItem(sku)
+            when (response) {
+                is APIResource.Success -> {
+                    _deleteInventory.emit(response.value)
+                }
+                is APIResource.Error -> {
+                    _errorMessage.emit(parseErrors(response))
+                }
+                else->{
+
+                }
+            }
+        }
+    }
 
 
 }
