9a1dd0566ae9f5e4655bc69fb699c2f36c719627
malcolmmaima <malcolmmaima@gmail.com>
2022-05-05 08:53:01 +0300

    fix

--- app/src/main/java/com/ultratechies/ghala/ui/warehouses/EditWarehouseBottomSheetFragment.kt
+++ app/src/main/java/com/ultratechies/ghala/ui/warehouses/EditWarehouseBottomSheetFragment.kt
@@ -165,7 +165,7 @@ class EditWarehouseBottomSheetFragment(var refreshListCallback: () -> Unit) :
     private fun submitEditedWarehouse() {
         // validate
         if (binding.etWarehouseName.text.isNullOrEmpty()) {
-            binding.etWarehouseName.error = getString(R.string.err)
+            binding.etWarehouseName.error = getString(R.string.error_warehouse_name)
             return
         }
 
