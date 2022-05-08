1e2efeef4aec8e31a41e387a9e2359dd14ec7e74
Florence Njumbi <wambuinjumbi@gmail.com>
2022-05-08 20:33:01 +0300

    refactor: separation of concerns

--- app/src/main/java/com/ultratechies/ghala/di/AppModule.kt
+++ app/src/main/java/com/ultratechies/ghala/di/AppModule.kt
@@ -1,79 +1,17 @@
 package com.ultratechies.ghala.di
 
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import com.ultratechies.ghala.BuildConfig
-import com.ultratechies.ghala.data.repository.*
-import com.ultratechies.ghala.utils.BASE_URL
+import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    @Provides
-    @Singleton
-    fun providesOkhttp(): OkHttpClient {
-        val okhhtp = OkHttpClient.Builder()
-
-        if (BuildConfig.DEBUG) {
-            val logger = HttpLoggingInterceptor()
-            logger.setLevel(HttpLoggingInterceptor.Level.BODY)
-
-            okhhtp.addInterceptor(logger)
-        }
-        return okhhtp.build()
-    }
-
-    @Provides
-    @Singleton
-    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit
-        .Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
-        .client(okHttpClient)
-        .baseUrl(BASE_URL)
-        .build()
-
     @Singleton
     @Provides
-    fun providesWarehouseAPI(retrofit: Retrofit): WarehouseApi =
-        retrofit.create(WarehouseApi::class.java)
-
-    @Singleton
-    @Provides
-    fun orderService(retrofit: Retrofit) :OrdersApi = retrofit.create(OrdersApi::class.java)
-
-    @Singleton
-    @Provides
-    fun inventoryService(retrofit: Retrofit): InventoryApi = retrofit.create(InventoryApi::class.java)
-
-    @Singleton
-    @Provides
-    fun deliveryNotesService(retrofit: Retrofit): DeliveryNotesApi = retrofit.create(DeliveryNotesApi::class.java)
-
-    @Singleton
-    @Provides
-    fun provideWarehouseRepo(warehouseAPI: WarehouseApi): WarehouseRepository = WarehouseRepositoryImpl(warehouseAPI)
-
-    @Singleton
-    @Provides
-    fun providesOrders(ordersApi: OrdersApi): OrdersRepository = OrdersRepositoryImpl(ordersApi)
-
-    @Singleton
-    @Provides
-    fun providesInventoryRepo(inventoryApi: InventoryApi): InventoryRepository = InventoryRepositoryImpl(inventoryApi)
-
-    @Singleton
-    @Provides
-    fun providesDeliveryNotes( deliveryNotesApi: DeliveryNotesApi): DeliveryNotesRepository = DeliveryNotesRepositoryImpl(deliveryNotesApi)
+    fun providesGson(): Gson = Gson()
 }
