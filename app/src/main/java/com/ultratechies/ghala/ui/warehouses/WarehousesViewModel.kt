c31cb24a8decebfc4b80dd2fce42b7ef9d04801c
Florence Njumbi <wambuinjumbi@gmail.com>
2022-05-08 21:06:18 +0300

    feat: fetch warehouses logic

--- app/src/main/java/com/ultratechies/ghala/ui/warehouses/WarehousesViewModel.kt
+++ app/src/main/java/com/ultratechies/ghala/ui/warehouses/WarehousesViewModel.kt
@@ -7,17 +7,23 @@ import androidx.lifecycle.viewModelScope
 import com.ultratechies.ghala.data.models.responses.WarehousesResponse
 import com.ultratechies.ghala.data.repository.APIResource
 import com.ultratechies.ghala.data.repository.WarehouseRepository
+import com.ultratechies.ghala.utils.parseErrors
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
+
 @HiltViewModel
 class WarehousesViewModel @Inject constructor(
     private val repository: WarehouseRepository
-): ViewModel() {
+) : ViewModel() {
     private val _warehouses: MutableLiveData<APIResource<WarehousesResponse>> = MutableLiveData()
     val warehouses: LiveData<APIResource<WarehousesResponse>>
         get() = _warehouses
+
+    private val _errorMessage: MutableLiveData<String> = MutableLiveData()
+     val errorMessage: LiveData<String> get() =  _errorMessage
 
     private val _deletedWarehouseResponse: MutableLiveData<APIResource<Unit>> = MutableLiveData()
     val deletedWarehouseResponse: LiveData<APIResource<Unit>>
@@ -26,6 +32,23 @@ class WarehousesViewModel @Inject constructor(
     fun getWarehouses() = viewModelScope.launch {
         _warehouses.value = APIResource.Loading
         _warehouses.value = repository.getWarehouses()
+    }
+
+    fun fetchWarehouses() {
+        viewModelScope.launch {
+            val warehouseResponse = repository.getWarehouses()
+            when (warehouseResponse) {
+                is APIResource.Success -> {
+                    _warehouses.postValue(warehouseResponse)
+                }
+                is APIResource.Loading -> {
+
+                }
+                is APIResource.Error -> {
+                    _errorMessage.postValue(parseErrors(warehouseResponse))
+                }
+            }
+        }
     }
 
     fun deleteWarehouse(id: Int) = viewModelScope.launch {
