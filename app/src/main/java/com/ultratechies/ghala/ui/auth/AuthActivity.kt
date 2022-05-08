969fef9952510f763e95b4cb7e908867cfc8172a
Florence Njumbi <wambuinjumbi@gmail.com>
2022-05-08 20:34:40 +0300

    feature: add dependency injection annotations

--- app/src/main/java/com/ultratechies/ghala/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/ultratechies/ghala/ui/auth/AuthActivity.kt
@@ -1,12 +1,18 @@
 package com.ultratechies.ghala.ui.auth
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.ultratechies.ghala.databinding.ActivityAuthBinding
+import com.ultratechies.ghala.ui.auth.viewmodels.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAuthBinding
+
+    private val userViewModel: UserViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
