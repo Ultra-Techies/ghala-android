b2b6291cdd9a61a94b25dc809143bbdba41bf088
Florence Njumbi <wambuinjumbi@gmail.com>
2022-05-08 20:51:10 +0300

    feature: create network module

---
+++ app/src/main/java/com/ultratechies/ghala/di/NetworkModule.kt
@@ -0,0 +1,69 @@
+package com.ultratechies.ghala.di
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.ultratechies.ghala.BuildConfig
+import com.ultratechies.ghala.data.repository.*
+import com.ultratechies.ghala.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun providesOkhttp(): OkHttpClient {
+        val okhhtp = OkHttpClient.Builder()
+
+        if (BuildConfig.DEBUG) {
+            val logger = HttpLoggingInterceptor()
+            logger.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            okhhtp.addInterceptor(logger)
+        }
+        return okhhtp.build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit
+        .Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .client(okHttpClient)
+        .baseUrl(BASE_URL)
+        .build()
+
+    @Singleton
+    @Provides
+    fun providesWarehouseAPI(retrofit: Retrofit): WarehouseApi =
+        retrofit.create(WarehouseApi::class.java)
+
+    @Singleton
+    @Provides
+    fun providesUserService(retrofit: Retrofit): UserApi = retrofit.create(UserApi::class.java)
+
+    @Singleton
+    @Provides
+    fun orderService(retrofit: Retrofit): OrdersApi = retrofit.create(OrdersApi::class.java)
+
+    @Singleton
+    @Provides
+    fun inventoryService(retrofit: Retrofit): InventoryApi =
+        retrofit.create(InventoryApi::class.java)
+
+    @Singleton
+    @Provides
+    fun deliveryNotesService(retrofit: Retrofit): DeliveryNotesApi = retrofit.create(
+        DeliveryNotesApi::class.java
+    )
+}
