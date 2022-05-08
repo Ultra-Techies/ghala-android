648578fa9f1e9dfce3fb661be07e6937a73fb894
Florence Njumbi <wambuinjumbi@gmail.com>
2022-05-08 20:52:35 +0300

    feat: add new endpoint

--- app/src/main/java/com/ultratechies/ghala/data/repository/OrdersApi.kt
+++ app/src/main/java/com/ultratechies/ghala/data/repository/OrdersApi.kt
@@ -2,10 +2,16 @@ package com.ultratechies.ghala.data.repository
 
 import com.ultratechies.ghala.data.models.responses.orders.OrderResponseItem
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface OrdersApi {
 
     @GET("orders")
     suspend fun getOrders():List<OrderResponseItem>
+
+    @GET("warehouseorders/{id}")
+    suspend fun  getOrdersByWarehouseId(
+        @Path("id") warehouseId: Int
+    ): List<OrderResponseItem>
 
 }
