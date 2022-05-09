8ae1630eea64140f1844ad283edacde4075cee22
Florence Njumbi <wambuinjumbi@gmail.com>
2022-05-09 20:32:15 +0300

    feat: setup settings fragment

--- app/src/main/java/com/ultratechies/ghala/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ultratechies/ghala/ui/settings/SettingsFragment.kt
@@ -1,34 +1,73 @@
 package com.ultratechies.ghala.ui.settings
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ultratechies.ghala.R
+import androidx.fragment.app.Fragment
+import com.ultratechies.ghala.databinding.SettingsFragmentBinding
+import com.ultratechies.ghala.domain.models.UserModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = SettingsFragment()
-    }
+    private lateinit var binding: SettingsFragmentBinding
 
     private lateinit var viewModel: SettingsViewModel
+
+    private val userModel: UserModel? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.settings_fragment, container, false)
+        binding = SettingsFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        editFields()
+        validateFields()
+        getUserByIdListener()
+        getUserByIdErrorListener()
+        updateUserListener()
+        updateUserErrorListener()
+
+    }
+
+    private fun toggleLoading(displayLoading: Boolean){
+        if (displayLoading){
+
+        }
+        else{
+
+        }
+    }
+
+    private fun validateFields() {
+        TODO("Not yet implemented")
+    }
+
+    private fun editFields() {
+        TODO("Not yet implemented")
+    }
+
+    private fun updateUserErrorListener() {
+        TODO("Not yet implemented")
+    }
+
+    private fun updateUserListener() {
+        TODO("Not yet implemented")
+    }
+
+    private fun getUserByIdErrorListener() {
+        TODO("Not yet implemented")
+    }
+
+    private fun getUserByIdListener() {
+        TODO("Not yet implemented")
     }
 
 }
